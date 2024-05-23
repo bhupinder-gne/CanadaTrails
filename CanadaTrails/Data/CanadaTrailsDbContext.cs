076cf7b34634bc94e3e03cf02c4a978cb1270b17
Bhupinder Singh <bhupinder.singh@epssw.com>
2024-05-23 13:57:11 -0400

    Added Model and DbContext

---
+++ CanadaTrails/Data/CanadaTrailsDbContext.cs
@@ -0,0 +1,17 @@
+﻿using CanadaTrails.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+using NZWalks.API.Models.Doamin;
+
+namespace NZWalks.API.Data
+{
+    public class CanadaTrailsDbContext : DbContext
+    {
+        public CanadaTrailsDbContext(DbContextOptions dbContextOptions) : base(dbContextOptions)
+        {
+        }
+
+        public DbSet<Difficulty> Difficulties { get; set; }
+        public DbSet<Region> Regions { get; set; }
+        public DbSet<Walk> Walks { get; set; }
+    }
+}
